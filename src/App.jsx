f7019266e587c2c19882da7e5dfb5db82ff653d7
ramiqkhan <mramiqkhan66@gmail.com>
2026-07-21 12:11:38 -0700

    ramiq

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
+import Header from "./Component/Layout/Header";
 
-// import TopBar from './Component/Layout/TopBar'
-import { Link } from 'react-router-dom'
-import Header from './Component/Layout/Header'
 const App = () => {
   return (
     <div>
-      {/* <TopBar /> */}
-      
       <Header />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
